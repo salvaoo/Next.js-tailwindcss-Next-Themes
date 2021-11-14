35b622fe6003997de9fcb71e8835dd68ff2c663d
salvaoo <salvadorgilrosales@outlook.es>
2021-11-14 21:07:56 +0000

    changes icon

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
-import Header from '../components/Header'
+import ChangeDarkMode from '../components/ChangeDarkMode'
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>ODIXA EVENTS</title>
+        <title>TITLE</title>
         <link rel="icon" href="/favicon.ico" />
         {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -15,7 +15,7 @@ export default function Home() {
         /> */}
       </Head>
 
-      <Header />
+      <ChangeDarkMode />
     </div>
   )
 }
