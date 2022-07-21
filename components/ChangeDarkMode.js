5011567a07edf6ca858df22f8509f0f2e53360f7
SalvaGR <salvadorgilrosales@outlook.es>
2022-07-21 12:47:29 +0100

    :bug: Dependencies updated and errors fixed (#1)

--- components/ChangeDarkMode.js
+++ components/ChangeDarkMode.js
@@ -1,27 +1,38 @@
 import { HiSun, HiMoon } from "react-icons/hi";
-import { RiHome2Fill, RiUser3Fill } from "react-icons/ri";
 import { useTheme } from "next-themes";
+import { useState, useEffect } from 'react'
 
 function ChangeDarkMode() {
-  const { theme, setTheme } = useTheme();
+
+  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme } = useTheme()
+
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
 
   return (
-    <header className="absolute bg-[#121212] dark:bg-white rounded-full p-2 top-2 right-2">
+    <div className="absolute bg-[#121212] dark:bg-white rounded-full p-2 top-2 right-2">
       <div className="cursor-pointer">
-      {theme === "light" ? (
-        <HiMoon
-          onClick={() => setTheme("dark")}
-          className="h-8 w-8 text-white sm:mt-0"
-        />
-      ) : (
-        <HiSun
-          onClick={() => setTheme("light")}
-          className="h-8 w-8 text-gray-900 sm:mt-0"
-        />
-      )}
+        {theme == "light" ? (
+          <HiMoon
+            onClick={() => setTheme("dark")}
+            className="h-8 w-8 text-white sm:mt-0"
+          />
+        ) : (
+          <HiSun
+            onClick={() => setTheme("light")}
+            className="h-8 w-8 text-gray-900 sm:mt-0"
+          />
+        )}
       </div>
-    </header>
-  );
+    </div>
+  )
 }
 
 export default ChangeDarkMode;
